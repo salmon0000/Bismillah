c8d33d2e4ff005f2a1521a42338cac0fffa2f59a
salmon0000 <148361613+salmon0000@users.noreply.github.com>
2025-06-21 21:30:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+alert("assalamualaikum warahmatullahi wabarakatuh")
+alert("Hi Fara Aurin Salsabila 👋🏻");
+alert("Selamat ulang taun Faraa");
+alert("yang keberapa yaa?? wkwkwkwk");
+alert("intinya udah kepala dua ajaa");
+alert("moga diberi keberkah an umurnya, diberi kelancaran rezeki dan karir nya");
+alert("makin sayang juga ke orang tua dan dikelilingin orang-orang baik")
+alert("makin kuat buat ngadepin segala problem di dunia ini💪🏻");
+alert("hebat sih udah ngelaluin sampe sejauh ini✨");
+alert("dijaga semangat sama kesehatannya yaa💪🏻");
+alert("apalagi yaa... yaa makin positive vibes aja deh kedepanya wkwkwk");
+alert("udah jadi orang yg ngucapin terakhir belom buat hari ini??");
+alert("yaudah kl gitu terimakasih sudah melihat ini, wabillahi taufiq wal hidayah wassalamualaikum warahmatullahi wabarakatuh🙏🏻");
